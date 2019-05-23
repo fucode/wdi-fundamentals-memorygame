a9a808507c5c1a9aa1dc3d2f1a635c85ba24e20a
Faraz <farazuddinva@gmail.com>
2019-05-22 22:46:50 -0400

    update

--- memory_game/main.js
+++ memory_game/main.js
@@ -2,11 +2,13 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 var cardOne = cards[0];
-var cardTwo = cards[1];
+var cardTwo = cards[2];
 
 cardsInPlay.push(cardOne);
 cardsInPlay.push(cardTwo);
 
+console.log("User flipped" + " " + cardOne);
+console.log("User flipped" + " " + cardTwo);
 
 if (cardsInPlay[0] === cardsInPlay[1]) {
   alert("You found a match!");
