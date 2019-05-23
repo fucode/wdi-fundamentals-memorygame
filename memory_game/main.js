9dc33988ad2bcd0b9375c00379685f6af2b89fc1
Faraz <farazuddinva@gmail.com>
2019-05-22 22:45:10 -0400

    update

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,4 +1,15 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.")
+}
